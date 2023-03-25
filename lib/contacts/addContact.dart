fc7a7fe9b542c89cb632e486c907c93a912bc0ea
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2023-03-25 16:24:35 +0530

    add contacts

---
+++ lib/contacts/addContact.dart
@@ -0,0 +1,122 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lunch_partner/controller/contactController.dart';
+
+class AddContact extends StatefulWidget {
+  AddContact({Key? key, required this.db}) : super(key: key);
+  ContactController db;
+  @override
+  _AddContactState createState() => _AddContactState();
+}
+
+class _AddContactState extends State<AddContact> {
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController numberController = new TextEditingController();
+  TextEditingController addressController = new TextEditingController();
+  TextEditingController emailController = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      appBar: AppBar(
+
+        title: Text("Add Contact"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                // widget.db.delete(widget.country["id"]);
+                // Navigator.pop(context, true);
+              })
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: inputDecoration("Contact Name"),
+                controller: nameController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: inputDecoration("Contact Number"),
+                controller: numberController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: inputDecoration("Address"),
+                controller: addressController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: inputDecoration("Email"),
+                controller: emailController,
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        color: Colors.transparent,
+        child: BottomAppBar(
+          color: Colors.transparent,
+          child: ElevatedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
+              ),
+              onPressed: () {
+                widget.db.create(nameController.text, numberController.text, addressController.text, emailController.text);
+                Navigator.pop(context, true);
+              },
+              child: Text(
+                "Add",
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
+      ),
+    );
+  }
+
+  InputDecoration inputDecoration(String labelText) {
+    return InputDecoration(
+      focusColor: Colors.white,
+      labelStyle: TextStyle(color: Colors.black),
+      labelText: labelText,
+      fillColor: Colors.white,
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(25.0),
+        borderSide: BorderSide(
+          color: Colors.black,
+          width: 4.0,
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(25.0),
+        borderSide: BorderSide(
+          color: Colors.blueAccent,
+          width: 3.0,
+        ),
+      ),
+    );
+  }
+}
